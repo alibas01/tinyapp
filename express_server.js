105a905ac55216ca4f3620df210a27039b33bdad
Ali Bas <alibas01@gmail.com>
2021-01-18 15:23:20 -0500

    short url route added

--- express_server.js
+++ express_server.js
@@ -20,18 +20,16 @@ app.get("/urls", (req, res) => {
   res.render("pages/urls_index", templateVars);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("pages/urls_show", templateVars);
+});
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
- 
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
